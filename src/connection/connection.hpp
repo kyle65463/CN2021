3a5e11339f0b8b2186c3ee55a16251710ff633bc
kyle65463 <kyle65463@gmail.com>
2021-11-27 13:21:45 +0800

    minor refactor and add comments

--- src/connection/connection.hpp
+++ src/connection/connection.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sys/socket.h>
-#define HEADER_SIZE 16
+#define FILE_BUF_SIZE 1024
+#define HEADER_SIZE 16 // Use a string with `HEADER_SIZE` chars indicates a file's length
 using namespace std;
 
 class Connection
@@ -10,7 +11,7 @@ public:
 
     void sendMessage(const string &msg, int size = -1)
     {
-        if(size == -1)
+        if (size < 0)
             size = msg.size() + 1;
         send(fd, msg.c_str(), size, 0);
     }
@@ -18,9 +19,11 @@ public:
     string recvMessage(int size = -1)
     {
         char buf[256] = {};
-        if (size == -1)
+        if (size < 0)
             size = sizeof(buf);
         int ret = recv(fd, buf, size, 0);
+
+        // Check if the connection is down
         if (ret <= 0)
         {
             isDisconnected = true;
@@ -31,19 +34,22 @@ public:
 
     void sendFile(const string &filename)
     {
+        // Open the file and read its length
         FILE *fp = fopen(filename.c_str(), "rb");
-        int bytes_read;
-        char buffer[1024];
         fseek(fp, 0L, SEEK_END);
         int filelen = ftell(fp);
         fseek(fp, 0L, SEEK_SET);
 
+        // Send the file's length as header
         sendMessage(to_string(filelen), HEADER_SIZE);
+
+        // Read and send the file
+        char buffer[FILE_BUF_SIZE];
+        int bytesRead;
         while (!feof(fp))
         {
-            if ((bytes_read = fread(&buffer, 1, 1024, fp)) > 0) {
-                send(fd, buffer, bytes_read, 0);
-            }
+            if ((bytesRead = fread(&buffer, 1, FILE_BUF_SIZE, fp)) > 0)
+                send(fd, buffer, bytesRead, 0);
             else
                 break;
         }
@@ -52,17 +58,20 @@ public:
 
     void recvFile(const string &outname)
     {
-        size_t datasize = 1;
-        FILE *filefd = fopen(outname.c_str(), "wb");
+        // Open the file and receive header (file length) first
+        FILE *fp = fopen(outname.c_str(), "wb");
         int length = stoi(recvMessage(HEADER_SIZE));
+
+        // Recieve the file
+        char buffer[FILE_BUF_SIZE];
+        int bytesRead;
         while (length > 0)
         {
-            char text[1024];
-            datasize = recv(fd, text, sizeof(text), 0);
-            length -= datasize;
-            fwrite(&text, 1, datasize, filefd);
+            bytesRead = recv(fd, buffer, sizeof(buffer), 0);
+            length -= bytesRead;
+            fwrite(&buffer, 1, bytesRead, fp);
         }
-        fclose(filefd);
+        fclose(fp);
     }
 
     void login(const string &username)
